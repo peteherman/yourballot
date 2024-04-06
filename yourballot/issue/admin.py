9d0b46e4dc5c851c025e79e9018a2d588769ddfe
Peter Herman <20931612+peteherman@users.noreply.github.com>
2024-04-06 14:49:01 -0400

    Created detailed api view to fetch candidate similarity results

--- yourballot/issue/admin.py
+++ yourballot/issue/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from yourballot.issue.models import CandidateIssueQuestionOpinion, Issue, IssueQuestion
+from yourballot.issue.models import CandidateIssueQuestionOpinion, Issue, IssueQuestion, VoterIssueQuestionOpinion
 
 admin.site.register(Issue)
 admin.site.register(IssueQuestion)
 admin.site.register(CandidateIssueQuestionOpinion)
+admin.site.register(VoterIssueQuestionOpinion)
